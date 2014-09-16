592cd99b94d55426fe14c179cd935c5de8496434
Maria Subero <mariasuberop@gmail.com>
2014-09-16 15:10:12 -0430

    Estandarizando dutas

--- app/routes.php
+++ app/routes.php
@@ -22,14 +22,16 @@ Route::get('/', array(
 // Confide routes users
 	Route::get('/users/create', 				'UsersController@create');
 	Route::post('users', 						'UsersController@store');
-	Route::get('users/login', 					'UsersController@login');
 	Route::post('users/login', 					'UsersController@doLogin');
 	Route::get('users/confirm/{code}', 			'UsersController@confirm');
-	Route::get('users/forgot_password', 		'UsersController@forgotPassword');
+	
 	Route::post('users/forgot_password', 		'UsersController@doForgotPassword');
 	Route::get('users/reset_password/{token}',	'UsersController@resetPassword');
 	Route::post('users/reset_password',			'UsersController@doResetPassword');
-	Route::get('users/logout', 					'UsersController@logout');
+	
+	Route::get('forgot', 						array('as' => 'forgot',			'uses' => 'UsersController@forgotPassword'));
+	Route::get('logout', 						array('as' => 'logout',			'uses' => 'UsersController@logout'));
+	Route::get('login', 						array('as' => 'login',			'uses' => 'UsersController@login'));
 
 
 //  Movies routes
